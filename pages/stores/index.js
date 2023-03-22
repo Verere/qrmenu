44b437b7efe5837df1a3f2dde723e84dd9d9d08a
Verere <linno4inno@gmail.com>
2023-03-22 11:05:41 +0100

    qr

--- pages/stores/index.js
+++ pages/stores/index.js
@@ -5,27 +5,27 @@ import { useRouter } from 'next/router'
 
 const Store = ({stores}) => {
 
-    // const [loaded,setLoaded] = useState(false)
-    // const Router = useRouter()
+    const [loaded,setLoaded] = useState(false)
+    const Router = useRouter()
 
-    // useEffect(() => {
-    //     const {pathname} = Router
-    //     // conditional redirect
-    //     if(pathname == '/stores'  ){
-    //         // with router.push the page may be added to history
-    //         // the browser on history back will  go back to this page and then forward again to the redirected page
-    //         // you can prevent this behaviour using location.replace
-    //         Router.push('/')
+    useEffect(() => {
+        const {pathname} = Router
+        // conditional redirect
+        if(pathname == '/stores'  ){
+            // with router.push the page may be added to history
+            // the browser on history back will  go back to this page and then forward again to the redirected page
+            // you can prevent this behaviour using location.replace
+            Router.push('/')
 
-    //        //location.replace("/hello-nextjs")
-    //     }else{
-    //         setLoaded(true)
-    //     }
-    //   },[]);
+           //location.replace("/hello-nextjs")
+        }else{
+            setLoaded(true)
+        }
+      },[]);
 
-    // if(!loaded){
-    //     return <div></div> //show nothing or a loader
-    // }
+    if(!loaded){
+        return <div></div> //show nothing or a loader
+    }
 
    
      
